a5d7a5e163a151f4667834e70e73a7b62d925ba5
Izzy Stannett <izzystannett@gmail.com>
2018-12-02 11:04:57 +0000

    Bottom Nav setup

--- app/src/main/java/uk/airbyte/fwc/MainActivity.java
+++ app/src/main/java/uk/airbyte/fwc/MainActivity.java
@@ -1,11 +1,15 @@
 package uk.airbyte.fwc;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +19,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView bottomNavigation = (BottomNavigationView) findViewById(R.id.btm_navigation);
         NavHostFragment navHost = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.my_nav_host_fragment);
-        NavController navController = navHost.getNavController();
+        NavController navController = Navigation.findNavController(this, R.id.my_nav_host_fragment);
 
-
+        BottomNavigationView bottomNavigation = (BottomNavigationView) findViewById(R.id.btm_navigation);
+        NavigationUI.setupWithNavController(bottomNavigation, navController);
     }
 }
